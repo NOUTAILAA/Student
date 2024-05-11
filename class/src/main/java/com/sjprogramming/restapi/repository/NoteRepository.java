2b446c5918b0b18c6eebe306f15ba9909254974e
drisszamani <123237782+drisszamani@users.noreply.github.com>
2024-05-11 16:02:56 +0100

    Delete class directory

--- class/src/main/java/com/sjprogramming/restapi/repository/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sjprogramming.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sjprogramming.restapi.entity.Note;
-
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
--- class/src/main/java/com/sjprogramming/restapi/repository/RemarqueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sjprogramming.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sjprogramming.restapi.entity.Remarque;
-
-public interface RemarqueRepository extends JpaRepository<Remarque, Long> {
-}
-
